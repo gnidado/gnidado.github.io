3e7a22a9cefb0000c394224269a117b423f40ff2
gnidado <38378548+gnidado@users.noreply.github.com>
2018-06-15 19:37:55 +0500

    + adaptive carousel, + active courses

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -9,6 +9,24 @@ function changeCourse(course) {
     toggleActive(`#${currentCourse}`, `#${course}`)
     $('#masters').css('background-image', `url('images/courses/${course}.jpg')`);
     currentCourse = course;
+    $('.courseNav li a').css('color', '#ffffff')
+    switch (course) {
+        case 'sport':
+            $('.courseNav li:nth-child(1) a').css('color', '#a082f3')
+            break;
+        case 'english':
+            $('.courseNav li:nth-child(2) a').css('color', '#a082f3')
+            break;
+        case 'networking':
+            $('.courseNav li:nth-child(3) a').css('color', '#a082f3')
+            break;
+        case 'digital':
+            $('.courseNav li:nth-child(4) a').css('color', '#a082f3')
+            break;
+    
+        default:
+            break;
+    }
 }
 
 let mastersId = ['#bizukova', '#orlova', '#maslenikov'];
@@ -33,6 +51,7 @@ function prevMaster() {
 let changeLoop = setInterval(nextMaster, 8000);
 
 $(function () {
+    $('.courseNav li:nth-child(1) a').css('color', '#a082f3')
     $('.masterWrapper > i:nth-child(2)').click(() => nextMaster());
     $('.masterWrapper > i:nth-child(1)').click(() => prevMaster());
 
